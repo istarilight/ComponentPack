58d98233dfe7162bedbbe42dc70947e5573acb61
Hailong Li <hailongli@hailongs-macbook-pro.local>
2018-12-24 02:34:38 -0500

    Initial Commit

---
+++ ComponentPack/ComponentPack.h
@@ -0,0 +1,19 @@
+//
+//  ComponentPack.h
+//  ComponentPack
+//
+//  Created by Hailong Li on 12/24/18.
+//  Copyright © 2018 Hailong Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ComponentPack.
+FOUNDATION_EXPORT double ComponentPackVersionNumber;
+
+//! Project version string for ComponentPack.
+FOUNDATION_EXPORT const unsigned char ComponentPackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ComponentPack/PublicHeader.h>
+
+
